d8ede8c642a2f244f4fd4ce95a5e643eec308f48
M CHARAN <medasanicharan66@gmail.com>
2026-03-10 07:09:22 +0530

    shell-script-disadvantages

--- why-ansible/main.sh
+++ why-ansible/main.sh
@@ -4,8 +4,8 @@ for server in $(cat servers.txt)
 do
    echo "Processing $server"
 
-   scp -i aws.pem shell-roboshop/mongo.repo shell-roboshop/mongodb.sh ec2-user@$server:/tmp/
+   scp shell-roboshop/mongo.repo shell-roboshop/mongodb.sh ec2-user@$server:/tmp/
 
-   ssh -i aws.pem ec2-user@$server "cd /tmp && sudo sh mongodb.sh"
+   ssh ec2-user@$server "cd /tmp && sudo sh mongodb.sh"
 
 done
