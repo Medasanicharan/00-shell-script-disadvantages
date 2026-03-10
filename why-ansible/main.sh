862061cb84d576574ff4f0f5c1cbe0e0a7270945
M CHARAN <medasanicharan66@gmail.com>
2026-03-10 07:21:57 +0530

    shell-script-disadvantages

--- why-ansible/main.sh
+++ why-ansible/main.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-# for server in $(cat servers.txt)
-# do
-#    echo "Processing $server"
-
-#    scp mongo.repo mongodb.sh ec2-user@$server:/tmp/
-
-#    ssh ec2-user@$server "cd /tmp && sudo sh mongodb.sh"
-
-# done
-
 for server in $(cat servers.txt)
 do
-(
+   echo "Processing $server"
+
    scp mongo.repo mongodb.sh ec2-user@$server:/tmp/
 
    ssh ec2-user@$server "cd /tmp && sudo sh mongodb.sh"
-) &
+
 done
-wait
